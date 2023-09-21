9382ee9ae1408206656e0eeb9331345c3352ac4d
Jose Manuel Fernandez Expolio <josemanuel.fernandez@panel.es>
2023-09-21 11:38:55 +0200

    CRUD Category

---
+++ Sales.API/Controllers/CategoriesController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Share.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public CategoriesController(DataContext dataContext) => _dataContext = dataContext;
+
+        [HttpGet]
+        public async Task<ActionResult> GetAsync()
+        {
+            return Ok(await _dataContext.Categories.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Category category)
+        {
+            try
+            {
+                _dataContext.Add(category);
+                await _dataContext.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate") ||
+                    dbUpdateException.InnerException!.Message.Contains("duplicada"))
+                {
+                    return BadRequest($"La categoría {category.Name} ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var category = await _dataContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Category category)
+        {
+            try
+            {
+                _dataContext.Update(category);
+                await _dataContext.SaveChangesAsync();
+                return Ok(category);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate") ||
+                   dbUpdateException.InnerException!.Message.Contains("duplicada"))
+                {
+                    return BadRequest($"La categoría {category.Name} ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var category = await _dataContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _dataContext.Remove(category);
+            await _dataContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+
+}
